10dcaa52557fcd37769d76ef34c9d625c656e275
SaradaPrasadSwain <somanath2486@gmail.com>
2026-01-30 02:34:35 +0530

    updated frontend code

--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -1,23 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { IconMenu2, IconShoppingCart, IconUserCircle } from '@tabler/icons-react';
 
 const Navbar = () => {
+  const links = [
+    { name: 'Mens', href: '/men' },
+    { name: 'Womens', href: '/women' },
+    { name: 'Children', href: '/children' },
+    { name: 'Groceries', href: '/groceries' },
+  ];
+
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div className='relative'>
-        <div className='flex  relative justify-center items-center bg-white mx-auto px-2 py-2 mt-2'>
-          <div className='bg-red-500'>Indiano</div>
-          <div className='flex'>
-            <Link to="/">Home</Link>
-            <Link to="/contact">contact us</Link>
-            <Link to="/xyz">xyz</Link>
-            <Link to="/profile">profile</Link>
+        <div className='flex relative justify-between items-center bg-white mx-auto px-2 py-2 mt-2'>
+          <button onClick={() => setOpen(!open)}>
+            <IconMenu2 />
+          </button>
+
+          {open && (
+            <div className='absolute inset-x-0 bg-blue-50 rounded-md top-10 px-2 py-2 mx-auto'>
+              <div className='flex flex-col items-start gap-4 text-sm text-neutral-500 p-4'>
+                {links.map((link, index) => (
+                  <Link to={link.href} key={index}>
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
+
+          <Link to="/">Indiano</Link>
+
+          <div className='flex gap-3'>
+            <Link to='/profile'>
+              <IconUserCircle />
+            </Link>
+            <Link to="/kart">
+              <IconShoppingCart />
+            </Link>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
